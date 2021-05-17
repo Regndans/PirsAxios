eec5a3ebfe9363bb859d0d81861a5ec88231a0d2
Morten Lyngberg Pedersen <mort73h9@edu.easj.dk>
2021-05-17 10:29:04 +0200

    Lavet put metode samt flot formatering af knap

--- js/index.js
+++ js/index.js
@@ -1,24 +1,29 @@
 const motionUrl = "https://restsense.azurewebsites.net/api/Motion/"
 const sensorUrl = "https://restsense.azurewebsites.net/api/Sensor/"
+const timerUrl = "https://restsense.azurewebsites.net/api/Timer/"
 
 Vue.createApp({
     data() {
         return {
             motions: [],
             sensors: [],
+            timers: [],
             getdata: null,
-            timerStart: new Date(Date.now()).toISOString().substr(0,16),
+            timerStart: null,
             timerEnd: null,
-            timerEndReal: new Date(Date.now()).toISOString().substr(0,16),
+            timerEndReal: null,
+            putData: null,
         }
     },
 
-    
+
 
     async created() {
         console.log("Created method called")
         this.HelperGetMotions(motionUrl)
         this.HelperGetSensors(sensorUrl)
+        this.HelperGetTimer(timerUrl)
+
     },
 
     methods: {
@@ -30,8 +35,8 @@ Vue.createApp({
         GetAllSensors() {
             this.HelperGetSensors(sensorUrl)
         },
-         //sets motion table to null
-         HideMotionTable() {
+        //sets motion table to null
+        HideMotionTable() {
             this.motions = []
         },
         // method for getting motions based on url
@@ -53,16 +58,24 @@ Vue.createApp({
                 alert(ex.message)
             }
         },
+        async HelperGetTimer(url) {
+            try {
+                const reponse = await axios.get(url)
+                this.timers = await reponse.data
+            } catch (ex) {
+                alert(ex.message)
+            }
+        },
         // method for switching active on/off 
         async Switchtruefalse(id) {
             try {
-                await this.HelperGetSensor(sensorUrl,id)
+                await this.HelperGetSensor(sensorUrl, id)
                 if (this.getdata.active) {
                     this.getdata.active = false
-                    const response = await axios.put(sensorUrl + id+"?key=4000", this.getdata)
+                    const response = await axios.put(sensorUrl + id + "?key=4000", this.getdata)
                 } else {
                     this.getdata.active = true
-                    const response = await axios.put(sensorUrl + id+"?key=4000", this.getdata)
+                    const response = await axios.put(sensorUrl + id + "?key=4000", this.getdata)
                 }
             }
             catch (ex) {
@@ -77,7 +90,7 @@ Vue.createApp({
                     const element = this.sensors[index];
                     if (element.active) {
                         element.active = false
-                        const response = await axios.put(sensorUrl + element.sensorId+"?key=4000", element)
+                        const response = await axios.put(sensorUrl + element.sensorId + "?key=4000", element)
                     }
                 }
             } catch (ex) {
@@ -92,7 +105,7 @@ Vue.createApp({
                     const element = this.sensors[index];
                     if (!element.active) {
                         element.active = true
-                        const response = await axios.put(sensorUrl + element.sensorId+"?key=4000", element)
+                        const response = await axios.put(sensorUrl + element.sensorId + "?key=4000", element)
                     }
                 }
             } catch (ex) {
@@ -101,18 +114,17 @@ Vue.createApp({
             this.HelperGetSensors(sensorUrl)
         },
         //method to get a single sensor
-        async HelperGetSensor(url,id) {
+        async HelperGetSensor(url, id) {
             try {
-                const reponse = await axios.get(url+id)
+                const reponse = await axios.get(url + id)
                 this.getdata = await reponse.data
             } catch (ex) {
                 alert(ex.message)
             }
         },
         //Method for determining name of sensor
-        DetermineName(sensid)
-        {
-            for(let x of this.sensors){
+        DetermineName(sensid) {
+            for (let x of this.sensors) {
                 if (sensid == x.sensorId) {
                     return x.sensorName
                 }
@@ -132,17 +144,25 @@ Vue.createApp({
             }
             */
         },
+        async PutTimes() {
+            try {
+                this.timers[0].activeStart = this.timerStart
+                this.timers[0].activeEnd = this.timerEndReal
+                const response = await axios.put(timerUrl+1,this.timers[0])
+                putData = await response.data
+            } catch (error) {
+                alert(error)
+            }
+        },
 
-        SensorOnOff (on)
-        {
-            if(on){return "On"}
+        SensorOnOff(on) {
+            if (on) { return "On" }
             else return "Off"
         },
 
-        FormatTime (time)
-        {
+        FormatTime(time) {
             date = new Date(time).toLocaleDateString("en-DK");
-            time = new Date(time).toLocaleTimeString("en-DK").replace(".",":").substr(0,5)
+            time = new Date(time).toLocaleTimeString("en-DK").replace(".", ":").substr(0, 5)
             return date + " kl. " + time
         },
     }
